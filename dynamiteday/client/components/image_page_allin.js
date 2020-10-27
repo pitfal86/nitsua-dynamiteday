8040bdaa00bbbb77904c1234968d203d9d3162bd
thomasarchuleta <pitfal86@hotmail.com>
2020-10-27 06:46:10 -0600

    Fixed dates

--- dynamiteday/client/components/image_page_allin.js
+++ dynamiteday/client/components/image_page_allin.js
@@ -8,19 +8,19 @@ const ImagePage = (props) => {
   return (
     <>
       <div className="grid-container-page">
-        <div>The Proposal<br />10-10-2021</div>
+        <div>The Proposal<br />10-10-2020</div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603762578/dynamitedayphotos/IMG_0153_Copy_okoddt.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603762631/dynamitedayphotos/IMG_0159_bn577i.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603762602/dynamitedayphotos/IMG_0158_wixna2.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603762666/dynamitedayphotos/IMG_0175_p3ihcy.jpg" className='img-rounded img-responsive' /></div>
       </div>
       <div className="grid-container-page">
-        <div>Engagement Photo Shoot<br />10-19-2021</div>
+        <div>Engagement Photo Shoot<br />10-19-2020</div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603521922/dynamitedayphotos/SaveTheDate_ryivpy.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603510707/dynamitedayphotos/All-In_xuix2o.jpg" className='img-rounded img-responsive' /></div>
       </div>
       <div className="grid-container-page">
-        <div>Venue Shopping - Cielo at Castle Pines<br />10-24-2021</div>
+        <div>Venue Shopping - Cielo at Castle Pines<br />10-24-2020</div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603766862/dynamitedayphotos/IMG_0201_splsdf.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603766862/dynamitedayphotos/IMG_0206_ulkwpb.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603766862/dynamitedayphotos/IMG_0203_thg2no.jpg" className='img-rounded img-responsive' /></div>
@@ -33,7 +33,7 @@ const ImagePage = (props) => {
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603766862/dynamitedayphotos/IMG_0205_fwphe5.jpg" className='img-rounded img-responsive' /></div>
       </div>
       <div className="grid-container-page">
-        <div>Venue Shopping - The Barn at Racoon Creek<br />10-25-2021</div>
+        <div>Venue Shopping - The Barn at Racoon Creek<br />10-25-2020</div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603767120/dynamitedayphotos/IMG_0215_uodz8l.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603767120/dynamitedayphotos/IMG_0213_bctlxg.jpg" className='img-rounded img-responsive' /></div>
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603767121/dynamitedayphotos/IMG_0216_sir7ub.jpg" className='img-rounded img-responsive' /></div>
@@ -46,10 +46,10 @@ const ImagePage = (props) => {
         <div><img src="https://res.cloudinary.com/dz7kvpuzo/image/upload/v1603767121/dynamitedayphotos/IMG_0217_w8q2sv.jpg" className='img-rounded img-responsive' /></div>
       </div>
       <div className="grid-container-page">
-        <div>Venue Shopping - Cheyenne Mountain Resort and Crooked Willow Farms<br />10-29-2021</div>
+        <div>Venue Shopping - Cheyenne Mountain Resort and Crooked Willow Farms<br />10-29-2020</div>
       </div>
       <div className="grid-container-page">
-        <div>Venue Picked<br />10-31-2021</div>
+        <div>Venue Picked<br />10-31-2020</div>
       </div>
       <div className="grid-container-page">
         <div>Invitations Picked<br />-TODO-</div>
